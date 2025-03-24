923c3d411a781a357b7bac686d0fb61720a08d7c
Teju <141817487+TejasriR@users.noreply.github.com>
2025-03-24 11:41:38 +0530

    Update pysparkrealtime1 2025-03-14 11_00_39.py

--- pysparkrealtime1 2025-03-14 11_00_39.py
+++ pysparkrealtime1 2025-03-14 11_00_39.py
@@ -3,43 +3,37 @@
 # MAGIC # Incremental Data Load in Databricks
 # MAGIC # Source files added in github repo
 
-# COMMAND ----------
+# Reading CSV file from DBFS
 
 df=spark.read.format("csv").option("header",True).option("inferschema",True).load("dbfs:/FileStore/rawsourcedata/sales_data_first.csv")
 df.display()
 
-# COMMAND ----------
-
+# Using Autoloader for continuous processing of files from DBFS.
 # MAGIC %md
 # MAGIC # Autoloader
 
-# COMMAND ----------
+# Streaming Data Ingestion Using Auto Loader
+#The given PySpark Auto Loader code is used for continuous ingestion of CSV files from DBFS (Databricks File System) into a structured streaming DataFrame.
 
 df= spark.readStream.format("cloudFiles").option("cloudFiles.format","csv").option("cloudFiles.schemalocation","dbfs:/FileStore/rawdestination/checkpoint").load("dbfs:/FileStore/rawsourcedata/")
 
-# COMMAND ----------
-
-# MAGIC %md 
-# MAGIC ## # Third file having different schema
-
-# COMMAND ----------
+# This PySpark Structured Streaming code writes data to a Delta Lake table on DBFS (Databricks FileStore) using triggered processing.
 
 df.writeStream.format("delta").option("checkpointlocation","dbfs:/FileStore/rawdestination/checkpoint").trigger(processingTime= " 3 seconds").start("dbfs:/FileStore/rawdestination/data")
 
-# COMMAND ----------
+# Data viewing in sql command
 
-# MAGIC %sql
-# MAGIC select * from delta. `dbfs:/FileStore/rawdestination/data`
+%sql
+select * from delta. `dbfs:/FileStore/rawdestination/data`
 
-# COMMAND ----------
+
 
 # MAGIC %md
 # MAGIC ## # You have a pipeline that frequently ingests data with a 'new schema', and you need to process it as soon as it arrives(Use mergeSchema)
-
-# COMMAND ----------
+##The mergeSchema option in Delta Lake is used when writing data to a Delta table to automatically update the schema if new columns appear in the incoming data.
 
 df.writeStream.format("delta").option("checkpointlocation","dbfs:/FileStore/rawdestination/checkpoint").trigger(processingTime= " 3 seconds").option("mergeSchema",True).start("dbfs:/FileStore/rawdestination/data")
 
-# COMMAND ----------
+
 
 
